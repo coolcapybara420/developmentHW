4f562c367011e5f4b31f36195f992c0ee6b1da7e
coolcapybara420 <michael_w_xu+cs1300@brown.edu>
2022-11-29 03:06:25 -0500

    Extra stuff

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import BakeryItem from './components/BakeryItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -21,6 +25,9 @@ function App() {
   const [breadFilter, setBreadFilter] = useState(false);
   const [pastryFilter, setPastryFilter] = useState(false);
   const [currentItems, setCurrentItems] = useState(bakeryData);
+  let currBread = breadFilter;
+  let currPastry = pastryFilter;
+  const [sort, setSort] = useState("price");
   function setCart(index) {
     let newCart = cartMap;
     if (index in cartMap) {
@@ -29,22 +36,49 @@ function App() {
       newCart[index] = 1;
     }
     setCartMap(newCart);
+    console.log(newCart);
+  }
+
+  function removeFromCart(index) {
+    let newCart = cartMap;
+    if (newCart[index] > 1) {
+      newCart[index] -= 1;
+    } else {
+      delete newCart[index];
+    }
+    setCartMap(newCart);
+    console.log(newCart);
   }
 
   const pastryChange = (event) => {
     setPastryFilter(event.target.checked);
+    currPastry = event.target.checked;
     const filteredData = bakeryData.filter(matchesFilterType);
-    console.log(filteredData);
     setCurrentItems(filteredData);
   };
 
   const breadChange = (event) => {
     setBreadFilter(event.target.checked);
+    currBread = event.target.checked;
+    const filteredData = bakeryData.filter(matchesFilterType);
+    setCurrentItems(filteredData);
   }
 
   const matchesFilterType = item => {
-    if (!pastryFilter) {
+    if (currPastry && !currBread) {
       if (item.type == "pastry") {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (!currPastry && currBread) {
+      if (item.type == "bread") {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (currBread && currPastry) {
+      if (item.type == "bread" && item.type == "pastry") {
         return true;
       } else {
         return false;
@@ -54,6 +88,14 @@ function App() {
     }
   }
 
+  const sortChange = (event) => {
+    setSort(event.target.value);
+  }
+
+  if (sort == "price") {
+    currentItems.sort((a,b) => a.price - b.price);
+  }
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
@@ -61,9 +103,24 @@ function App() {
       
       <FormControlLabel control={<Checkbox onChange={pastryChange}/>} label="Pastry" />
       <FormControlLabel control={<Checkbox onChange={breadChange}/>} label="Bread" />
+
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="price"
+        name="radio-buttons-group"
+        onChange={sortChange}
+        >
+        <FormControlLabel value="price" control={<Radio />} label="Price" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        </RadioGroup>
+      </FormControl>
       
       {currentItems.map((item, index) => {
-        return (<BakeryItem setCart={setCart} setTotalPrice={setTotalPrice} item={item} index={index} currPrice={totalPrice}/>)})
+        return (<BakeryItem cartMap={cartMap} setCart={setCart} setTotalPrice={setTotalPrice} item={item} 
+          index={index} currPrice={totalPrice} removeFromCart={removeFromCart}/>)})
         }
 
       <div>
@@ -75,18 +132,10 @@ function App() {
           </div>
         )})}
         {/* TODO: render a list of items in the cart */}
-        <p>{"Total Cost: " + totalPrice.toFixed(2)}</p>
+        <p>{"Total Cost: " + Math.abs(totalPrice).toFixed(2)}</p>
       </div>
     </div>
   );
-}
-
-function selectFilterType() {
-
-}
-
-function matchesFilterType() {
-
 }
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,4 +1,20 @@
+import "../styles/BakeryItem.css";
+
 export default function BakeryItem(props) {
+    if (props.index in props.cartMap) {
+        return (
+            <div class="BakeryItem">
+                <img src={props.item.image}></img>
+                <h2>
+                {props.item.name}
+                </h2> <br></br>
+                <p>{props.item.description}</p>
+                <p>{props.item.price}</p>
+                <button onClick={() => {props.setTotalPrice(props.currPrice + props.item.price); props.setCart(props.index);}}>Add to Cart</button>
+                <button onClick={() => {props.setTotalPrice(props.currPrice - props.item.price); props.removeFromCart(props.index);}}>Remove from Cart</button>
+            </div>
+        );
+    } else {
     return (
         <div class="BakeryItem">
             <img src={props.item.image}></img>
@@ -10,4 +26,5 @@ export default function BakeryItem(props) {
             <button onClick={() => {props.setTotalPrice(props.currPrice + props.item.price); props.setCart(props.index);}}>Add to Cart</button>
         </div>
     );
+    }
   }
